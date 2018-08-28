2480efb2baa9ab6a951961f370c30d597bd63c38
Alexander F Rødseth <rodseth@gmail.com>
2018-08-28 01:42:12 -0700

    Use which to locate sbcl

--- lisp.sh
+++ lisp.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+#
+# SBCL wrapper script
+#
+# See https://github.com/xyproto/lisp for more information.
+#
+
 filename="$1"
 mainfunction="${2:-main}"
+
 if [ -z $filename ]; then
   echo 'Please provide a filename as the first argument'
   exit 1
@@ -9,11 +16,18 @@ if [ ! -f $filename ]; then
   echo "Could not find the file named \"$filename\""
   exit 1
 fi
-if [ ! -x /usr/bin/sbcl ]; then
-  echo '/usr/bin/sbcl is not executable'
+
+sbcl_path=$(which sbcl)
+if [ -z $sbcl_path ]; then
+  echo 'sbcl is not in the PATH environment variable'
   exit 1
 fi
-/usr/bin/sbcl \
+if [ ! -x $sbcl_path ]; then
+  echo "$sbcl_path is not executable"
+  exit 1
+fi
+
+"$sbcl_path" \
   --noinform \
   --noprint \
   --no-userinit \
